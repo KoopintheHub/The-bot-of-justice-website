ee9dc56cb584abe4ee4f7e4607cdaf9d139fec78
Koop <61279093+KoopintheHub@users.noreply.github.com>
2020-10-18 16:37:48 +0100

    Update disable.js

--- content/disable.js
+++ content/disable.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
   $('body').on('contextmenu', function() {
     return false;
   });
-  $('code').on('contextmenu', function() {
-    return true;
-  });
   $('body').on('dragstart', function() {
     return false;
   });
