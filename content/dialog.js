4289580cd4be6ca548a95d7e7011749533a3142d
Koop <61279093+KoopintheHub@users.noreply.github.com>
2020-10-25 06:44:48 +0000

    Update dialog.js

--- content/dialog.js
+++ content/dialog.js
@@ -5,10 +5,10 @@ function displayDialog(width, height, string, buttonNum) {
   if (!darken) {
     darken = true;
     allowToPressButton = false;
-    darkenEl.style = "tpointer-events: all; background: rgba(0,0,0,0.5); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15; transition: 0.5s";
+    darkenEl.style = "tpointer-events: all; background: rgba(0,0,0,0.5); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15;";
     darkenEl.innerHTML = "<div class=\"border\" style=\"opacity: 1; display: block; width: 50%; height: 50%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);\">" + string + "<button onclick=\"displayDialog()\" style=\"width: calc(100% - 11px); position: absolute; bottom: 5px; left: 5px\">OK</button></div>";
-  $('#darken').fadeOut(0);
-  $('#darken').fadeIn(150);
+    $('#darken').fadeOut(0);
+    $('#darken').fadeIn(150);
   } else {
     darken = false;
     allowToPressButton = true;
