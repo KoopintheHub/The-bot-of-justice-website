4c9181a3e45b5f85c739b8c2ba2b6a13d24fde52
Koop <61279093+KoopintheHub@users.noreply.github.com>
2021-01-04 17:34:17 +0000

    Update dialog.js

--- content/dialog.js
+++ content/dialog.js
@@ -1,19 +1,17 @@
 var darken = false;
 var darkenEl = document.getElementById('darken');
 
-function displayDialog(width, height, string, buttonNum) {
-  if (!darken) {
-    darken = true;
-    allowToPressButton = false;
-    darkenEl.style = "pointer-events: all; background: rgba(0,0,0,0.5); width: " + width + "%; height: " + height + "%; position: fixed; top: 0px; left: 0px; z-index: 15;";
-    darkenEl.innerHTML = "<div class=\"border\" style=\"opacity: 1; display: block; width: 50%; height: 50%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);\">" + string + "<button onclick=\"displayDialog()\" style=\"width: calc(100% - 11px); position: absolute; bottom: 5px; left: 5px\">OK</button></div>";
-    $('#darken').fadeOut(0);
-    $('#darken').fadeIn(150);
-  } else {
-    darken = false;
-    allowToPressButton = true;
-    /* darkenEl.style = "pointer-events: none; background: rgba(0,0,0,0); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15;";
-    darkenEl.innerHTML = ""; */
-    $('#darken').fadeOut(150);
-  }
+function displayDialog(width, height, innerHTML) {
+    if (!darken) {
+        darken = true;
+        document.getElementById('darken').style = "pointer-events: all; background: rgba(0,0,0,0.5); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15";
+        document.getElementById('darken').innerHTML = "<div style=\"opacity: 1; background-color: rgba(255, 255, 255, 0.5); border: 1px solid black; border-radius: 18px; display: block; width: " + width + "%; height: " + height + "%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);\"><div style=\"position: absolute; left: 6px; top: 3px;\">" + innerHTML + "</div></div>";
+        $('#darken').fadeOut(0);
+        $('#darken').fadeIn(150);
+    } else {
+        darken = false;
+        // document.getElementById('darken').style = "pointer-events: none; background: rgba(0,0,0,0); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15";
+        // document.getElementById('darken').innerHTML = "";
+        $('#darken').fadeOut(150);
+    }
 }
